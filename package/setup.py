828f8c1f1e5fc49f29ffc59e75532884d52e35ab
river8243 <84426227+river8243@users.noreply.github.com>
2024-11-28 16:50:21 +0800

    Update setup.py

--- package/setup.py
+++ package/setup.py
@@ -2,7 +2,7 @@ import setuptools
 setuptools.setup(name='speaker_separation',
                  version='1.0.2',
                  description='DPRNN model for speaker separation',
-                 author='esb18849',
+                 author='river',
                  author_email='a824300@gmail.com',
                  license='MIT',
                  install_requires=['numpy>=1.19.1', 'scipy==1.5.4', 'torch>=1.8.1', 'speechbrain>=0.5.4', 'torchaudio>=0.8.1', 'wget>=3.2'],
